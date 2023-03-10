2155d6477cc736e002db137106a93defbe8e72e1
Aaron Abbott <aaronabbott@google.com>
2023-03-10 21:07:20 +0000

    Revert "Make bike service slow and expensive"
    This reverts commit 49d03c92ea94e47ceae5d5b0fd6e4f782c482241.

--- examples/java-jfr/rideshare/src/main/java/org/example/rideshare/bike/BikeService.java
+++ examples/java-jfr/rideshare/src/main/java/org/example/rideshare/bike/BikeService.java
@@ -1,9 +1,5 @@
 package org.example.rideshare.bike;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import org.example.rideshare.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,12 +10,6 @@ public class BikeService {
     OrderService orderService;
 
     public void orderBike(int searchRadius) {
-        // Make order bike slow and consume tons of memory
-        var list = List.of("hello");
-        for (int i = 0; i < 100; ++i) {
-            list = Stream.concat(list.stream(), list.stream()).collect(Collectors.toList());
-        }
-
         orderService.findNearestVehicle(searchRadius, "bike");
     }
 }
