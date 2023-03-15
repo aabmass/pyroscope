c3ceb30765f019556b39d7c1a328cdb1ec8996f7
Aaron Abbott <aaronabbott@google.com>
2023-03-15 20:33:27 +0000

    Switch to pyroscope

--- examples/python/rideshare/fastapi/lib/server.py
+++ examples/python/rideshare/fastapi/lib/server.py
@@ -5,18 +5,14 @@ from lib.bike.bike import order_bike
 from lib.car.car import order_car
 from lib.scooter.scooter import order_scooter
 
-import googlecloudprofiler
+import pyroscope
 
-# Profiler initialization. It starts a daemon thread which continuously
-# collects and uploads profiles. Best done as early as possible.
-googlecloudprofiler.start(
-    service='fastapi-rideshare',
-    service_version='1.0.1',
-    # verbose is the logging level. 0-error, 1-warning, 2-info,
-    # 3-debug. It defaults to 0 (error) if not set.
-    verbose=3,
-    # project_id must be set if not running on GCP.
-    # project_id='my-project-id',
+pyroscope.configure(
+    application_name = "fastapi-rideshare",
+    server_address   = "http://10.128.0.2:4040",
+    tags = {
+        "version": "1.1.0",
+    },
 )
 
 app = FastAPI()
