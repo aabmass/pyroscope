e932433bf556590fd0afceacbfd65ff1666d89b1
Aaron Abbott <aaronabbott@google.com>
2023-03-15 20:26:32 +0000

    Add cloudprofiler lib and set up

--- examples/python/rideshare/fastapi/lib/server.py
+++ examples/python/rideshare/fastapi/lib/server.py
@@ -5,6 +5,19 @@ from lib.bike.bike import order_bike
 from lib.car.car import order_car
 from lib.scooter.scooter import order_scooter
 
+import googlecloudprofiler
+
+# Profiler initialization. It starts a daemon thread which continuously
+# collects and uploads profiles. Best done as early as possible.
+googlecloudprofiler.start(
+    service='fastapi-rideshare',
+    service_version='1.0.1',
+    # verbose is the logging level. 0-error, 1-warning, 2-info,
+    # 3-debug. It defaults to 0 (error) if not set.
+    verbose=3,
+    # project_id must be set if not running on GCP.
+    # project_id='my-project-id',
+)
 
 app = FastAPI()
 
